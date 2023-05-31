25372cdbcd1f9daa5cad8250211fbd2583e54846
Edward Lejeune <erwin.lejeune@tii.ae>
2023-05-31 11:02:37 +0400

    add instructions

---
+++ python/instructions.md
@@ -0,0 +1,14 @@
+### Python Instructions
+
+```bash
+python3 main.py -h
+    pygame 2.4.0 (SDL 2.26.4, Python 3.11.3)
+    Hello from the pygame community. https://www.pygame.org/contribute.html
+    usage: Boids Simulator [-h] [-c CONFIG]
+
+    A Simulation of the movements of bird flocks using the Boids genetic algorithm
+
+    options:
+    -h, --help            show this help message and exit
+    -c CONFIG, --config   Absolute path to default initial configuration
+```
--- python/main.py
+++ python/main.py
@@ -8,18 +8,35 @@ import argparse
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
+    main_parser = argparse.ArgumentParser(
         prog='Boids Simulator',
         description='A Simulation of the movements of bird flocks using the Boids genetic algorithm'
-    )   
-    parser.add_argument('config_path')
-
-    args = parser.parse_args()
+    )
+    main_parser.add_argument('-c', '--config', default="", help="Absolute path to default initial configuration")
+    args = main_parser.parse_args()
 
     data = dict()
 
-    with open(args.config_path) as f:
-        data = yaml.load(f, Loader=SafeLoader)
+    if args.config:
+        print("Using Parsed Configuration!")
+        with open(args.config) as f:
+            data = yaml.load(f, Loader=SafeLoader)
+    else:
+        print("Using Default Configuration!")
+        data = {
+            "boids": {
+                "width": 1000,
+                "height": 1000,
+                "num_boids": 80,
+                "max_speed": 3,
+                "separation_radius": 15,
+                "alignment_radius": 100,
+                "cohesion_radius": 100,
+                "separation_force": 0.8,
+                "alignment_force": 0.008,
+                "cohesion_force": 0.01
+            }
+        }
 
     print("==========")
     print("PARAMETERS")
